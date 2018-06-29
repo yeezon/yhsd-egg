38dce4e92fcb4554ec3e0e52858dd8f795d4fe43
Aurum <huangjin@yeezon.com>
2018-06-29 15:42:41 +0800

    enable crsf

--- config/config.default.js
+++ config/config.default.js
@@ -3,14 +3,24 @@
 module.exports = appInfo => {
   const config = {};
 
+  config.security = {
+    /**
+     * csrf开启时本地测试报过“invalid crsf token”，所以默认关掉
+     * 需要再在应用层面手动打开
+     */
+    csrf: {
+      enable: false,
+    },
+  };
   /**
-   * some description
+   * 单元测试使用的配置，与框架实际功能无关
    * @member Config#test
    * @property {String} key - some description
    */
   config.test = {
     key: appInfo.name + '_123456',
   };
+
 
   return config;
 };
